610f83770f88c7564b51f613ce60cada6f7a87d1
Jamie Allbright <jamie.allbright@aol.com>
2018-08-15 23:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Jamie"
-var height = "72"
+var height = 72
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseIt(height+1)
